1ec5c224e6644c9e0c4881b694aeffbba1cb379e
ryun89 <115915990+ryun89@users.noreply.github.com>
2024-03-12 14:05:42 +0900

    Initial Commit

---
+++ MedicalNoteApp/MedicalNoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedicalNoteAppApp.swift
+//  MedicalNoteApp
+//
+//  Created by 八久響 on 2024/03/12.
+//
+
+import SwiftUI
+
+@main
+struct MedicalNoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
